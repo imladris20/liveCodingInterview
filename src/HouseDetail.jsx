f64f021e95d2be92b04b15d0f33d83fe285becb5
Po Lien Lin <polien.joe.lin@gmail.com>
2024-10-16 17:30:15 +0800

    Fix: Adjust head line style

--- src/HouseDetail.jsx
+++ src/HouseDetail.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 const HouseDetail = () => {
   const { houseId } = useParams();
 
-  return <h1>here is house {houseId} detail </h1>;
+  return <h1 className="ml-10 mt-3">Here is house {houseId} detail </h1>;
 };
 
 export default HouseDetail;
