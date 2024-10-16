a1ebf4461d62a8b14b5eaeee80fcdf60bd786aa3
Po Lien Lin <polien.joe.lin@gmail.com>
2024-10-16 17:28:03 +0800

    Feat: 打API 印出來

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const getHouses = async () => {
+      const res = await fetch("http://localhost:3000/houses");
+
+      const data = await res.json();
+
+      console.log(data);
+    };
+
+    getHouses();
+  }, []);
+
   return (
     <>
       <header>
@@ -8,11 +22,11 @@ function App() {
         <div className="flex-row">
           <input
             type="text"
-            className="w-1/3 rounded-lg border border-solid border-violet-400 p-2 focus:border-violet-700 focus-visible:border-violet-700 text-slate-800"
+            className="w-1/3 rounded-lg border border-solid border-violet-400 p-2 text-slate-800 focus:border-violet-700 focus-visible:border-violet-700"
           />
           <button
             type="button"
-            className="ml-2 rounded-lg bg-violet-400 p-2 text-white hover:bg-violet-300 transition-colors duration-200 ease-in-out"
+            className="ml-2 rounded-lg bg-violet-400 p-2 text-white transition-colors duration-200 ease-in-out hover:bg-violet-300"
           >
             篩選
           </button>
