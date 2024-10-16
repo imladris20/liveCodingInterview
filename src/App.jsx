665e316737ceb569925d4bfd71b7096f9e09d703
Po Lien Lin <polien.joe.lin@gmail.com>
2024-10-16 15:54:06 +0800

    Feat: Add header and search area

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 function App() {
-
   return (
     <>
-      <h1>Let&apos;s Sale the house</h1>
+      <header>
+        <img src="logo.svg" alt="home logo" className="ml-3 mt-3" />
+      </header>
+      <section className="searchArea ml-3 mt-3">
+        <div className="flex-row">
+          <input
+            type="text"
+            className="w-1/3 rounded-lg border border-solid border-violet-400 p-2 focus:border-violet-700 focus-visible:border-violet-700 text-slate-800"
+          />
+          <button
+            type="button"
+            className="ml-2 rounded-lg bg-violet-400 p-2 text-white hover:bg-violet-300 transition-colors duration-200 ease-in-out"
+          >
+            篩選
+          </button>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
